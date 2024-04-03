49cd45ec3da9f11bd1081631bdcba633cdff5c4b
Taciany Maria S <138811627+tacianyalmeida@users.noreply.github.com>
2024-04-03 17:46:25 -0300

    Add files via upload

---
+++ App/App.jsx
@@ -0,0 +1,16 @@
+import Profile from '../Profile/Profile'
+import './App.css'
+import jessicaImage from '../assets/jessi'
+
+const App = () => {
+    return(
+        <main className="container_main">
+             <div className="container_card">
+                 <Profile 
+                 image={}/>  
+             </div>
+        </main>
+    )
+}
+
+export default App;
---
+++ Profile/Profile.jsx
@@ -0,0 +1,11 @@
+const Profile = ({image, name, location, role}) =>{
+return(
+    <>
+    <img src= {image} alt= {""} />
+    <h1>{name}</h1>
+    <p>{location}</p>
+    <p>{role}</p>
+    </>
+)
+}
+export default Profile
